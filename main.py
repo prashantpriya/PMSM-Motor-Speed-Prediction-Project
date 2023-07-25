63d559a30e0f501a8486ea359c51d7e3b503092c
prashantpriya <90123939+prashantpriya@users.noreply.github.com>
2023-07-25 23:10:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 22 22:47:48 2023
+
+@author: Admin
+"""
+
+import streamlit as st
+
+#st.sidebar.header("Predicting the PMSM speed")
+st.set_page_config(page_title='PMSM_speed Prediction')
+
+st.header("PERMANENT MAGNET SYNCHRONOUS MOTORS")
+#st.image("pmsm1.jpg")
+st.write("Permanent magnet synchronous motors (PMSM) are typically used for high-performance and high-efficiency motor drives. High-performance motor control is characterized by smooth rotation over the entire speed range of the motor, full torque control at zero speed, and fast acceleration and deceleration")
+#st.write("")
+#st.sidebar.success("PMSM speed Prediction")
+#st.sidebar.write("Predict for single input")
+#st.sidebar.write("Predict by uploading the file ")
